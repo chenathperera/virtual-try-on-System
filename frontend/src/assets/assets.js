c09cc42f26bb7f553223b9ee9b539d5e5edb8e16
Chenath Perera <chenathperera@gmail.com>
2025-11-29 20:53:04 +0530

    Nav bar frontend

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,22 @@
+import logo from './logo.png'
+import cart_icon from './cart_icon.svg'
+import serach_icon from './search_icon.svg'
+import profile_icon from './profile_icon.svg'
+import menu_icon  from './menu_icon.svg'
+import dropdown_icon from './dropdown_icon.svg'
+
+export const assets= {
+
+    logo,
+    cart_icon,
+    serach_icon,
+    profile_icon,
+    menu_icon,
+    dropdown_icon
+
+}
+
+export const products = [
+
+
+]
---
+++ frontend/src/components/Hero.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='flex flex-col sm:flex-row border border-grey-400'>
+        {/*hero left side*/}
+        <dive className='w-full'>
+
+
+        </dive>
+      
+    </div>
+  )
+}
+
+export default Hero
