9dccf9f94fdc592613334940e727549f26fc9fed
Chenath Perera <chenathperera@gmail.com>
2025-12-09 00:35:12 +0530

    add,remove,singleitem,list backend methods

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -8,6 +8,6 @@ const productRouter = express.Router();
 productRouter.post('/remove',removeProduct);
 productRouter.post('/add',upload.fields([{name:'image1',maxCount:1},{name:'image2',maxCount:1},{name:'image3',maxCount:1},{name:'image4',maxCount:1}]),addProduct);
 productRouter.post('/single',singleProduct);
-productRouter.post('/list',listProduct);
+productRouter.get('/list',listProduct);
 
 export default productRouter
