b0107dfd62b701026c2d00cd3bb77c966ca80b5f
Chenath Perera <chenathperera@gmail.com>
2025-12-09 03:01:37 +0530

    admin panel setup

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -3,6 +3,9 @@ import React from 'react'
 const App = () => {
   return (
     <div>
+       <div className="p-8 bg-blue-500 text-white font-bold">
+      Tailwind is working!
+    </div>
       
     </div>
   )
