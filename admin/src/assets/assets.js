63b820ac27378014f1f9a20174b5ca31b4884d26
Chenath Perera <chenathperera@gmail.com>
2025-12-09 12:55:04 +0530

    admin panel + login frontend

---
+++ admin/src/assets/assets.js
@@ -0,0 +1,17 @@
+
+
+import logo from './logo.png'
+import add from './add_icon.svg'
+import order from './order.svg'
+import list from './list.svg'
+
+
+
+
+export const assets = {
+    logo,
+    add,
+    order,
+    list,
+   
+}
