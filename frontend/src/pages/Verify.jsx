b3e72785a6cd63b24a6fb339324f2209d9eb8132
Chenath Perera <chenathperera@gmail.com>
2025-12-16 14:52:58 +0530

    payment verifiy

--- frontend/src/pages/Verify.jsx
+++ frontend/src/pages/Verify.jsx
@@ -1,6 +1,51 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ShopContext } from '../context/ShopContext';
+import { useSearchParams } from 'react-router-dom';
+import axios from 'axios';
+import { toast } from 'react-toastify';
 
 const Verify = () => {
+
+    const {navigate,token,setCartItem, backendUrl} =useContext(ShopContext);
+  const [ searchParams,setSearchParams] = useSearchParams()
+
+    const success = searchParams.get('success')
+    const orderId = searchParams.get('orderId')
+
+  const verifyPayment = async () =>{
+   
+
+    try {
+
+      if(!token){
+        return null
+      }
+
+      const response=   await axios.post(backendUrl + '/api/order/verifyStripe',{success,orderId},{headers:{token}})
+       if(response.data.success){
+                
+                                setCartItem({})
+                                navigate('/orders')
+                
+                            }else{
+                                console.log(error)
+                                toast.error(response.data.message)
+                               
+                            }
+
+      
+    } catch (error) {
+      
+    }
+  }
+
+  useEffect(()=>{
+
+    loadOrderData()
+
+  },[token])
+
+
   return (
     <div>
       
